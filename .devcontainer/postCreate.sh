eb1ba8b5bbe9a27a4748b3b2295236dd688095dc
David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>
2025-06-14 20:06:18 +0000

    Looks like non-privileged container user is not supported by GitHub Actions
    Signed-off-by: David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -15,32 +15,28 @@ case "${ID}" in
     ubuntu | debian)
         # Official `python` image is Debian based.
         export DEBIAN_FRONTEND=noninteractive
-        sudo apt-get install --no-install-recommends -y \
+        apt-get install --no-install-recommends -y \
 # TODO            docker-ce \
             gh
         ;;
     almalinux)
         # For RH* support we use AlmaLinux
 
-        sudo dnf install -y 'dnf-command(config-manager)'
+        dnf install -y 'dnf-command(config-manager)'
 
         # Configure Docker CE repo
-        sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-        
+        dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
         # Configure the GitHub repo for the gh client
-        sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
- 
+        dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
         # VS Code requires git so we do that in Dockerfile before we get here
-        sudo yum install --enablerepo crb -y \
+        yum install --enablerepo crb -y \
             docker-ce \
             gh
         ;;
     # Otherwise assume the container image comes fully loaded I guess
 esac
-
-sudo chgrp docker /var/run/docker.sock
-sudo usermod -a -G docker luser
-newgrp docker
 
 # Common stuff
 gh extension install nektos/gh-act
