2b48c999258cb454ffa9b97fd9b860ea3f5123b7
David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>
2025-08-10 00:35:17 +0000

    - `conan-toolchain-demo` repo split - image selection updates
    Signed-off-by: David L. Armstrong <1403560+daversomethingsomething@users.noreply.github.com>

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -16,27 +16,14 @@ case "${ID}" in
         # Official `python` image is Debian based.
         export DEBIAN_FRONTEND=noninteractive
         apt-get install --no-install-recommends -y \
-# TODO            docker-ce \
             gh
+# TODO            docker-ce \
+        gh extension install nektos/gh-act
         ;;
     almalinux)
         # For RH* support we use AlmaLinux
-
-        dnf install -y 'dnf-command(config-manager)'
-
-        # Configure Docker CE repo
-        dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-        # Configure the GitHub repo for the gh client
-        dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-
-        # VS Code requires git so we do that in Dockerfile before we get here
-        yum install --enablerepo crb -y \
-            docker-ce \
-            gh
         ;;
     # Otherwise assume the container image comes fully loaded I guess
 esac
 
 # Common stuff
-gh extension install nektos/gh-act
